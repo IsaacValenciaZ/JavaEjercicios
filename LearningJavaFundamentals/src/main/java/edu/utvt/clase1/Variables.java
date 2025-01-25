88ab55d782c69d6401d580d4cc9183c45441e4f7
IsaacValenciaZ <al222210520@gmail.com>
2025-01-25 00:00:08 -0600

    Ejercicios_18_01_25

---
+++ LearningJavaFundamentals/src/main/java/edu/utvt/clase1/Variables.java
@@ -0,0 +1,18 @@
+package edu.utvt.clase1;
+
+public class Variables {
+    public static void main(String[] args) {
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+
+        int intValue = 100;
+        System.out.println(intValue);
+
+        long hugeValue = 100000000L;
+        System.out.println(hugeValue);
+
+        float floatVar = 3.14f;
+        System.out.println(floatVar);
+
+    }
+}
